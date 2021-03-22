d26a5d88911e4ed528835f79902540719eea15f4
agnespet <agnespet@kth.se>
2021-03-22 15:50:53 +0100

    fixed title img bug, created css folder

--- src/views/titelView.js
+++ src/views/titelView.js
@@ -1,3 +1,4 @@
+import '../css/titleView.css';
 import logo from '../images/wonderlust.png';
 import logo1 from '../images/wonderlust1.png';
 import logo2 from '../images/wonderlust2.png';
@@ -5,15 +6,15 @@ import logo2 from '../images/wonderlust2.png';
 
 function TitleView(){
     return(
-        <header class="header">
-            <div class="container">
+        <div>
+            <div class="title-container">
                 <img src={logo2}/>
                 <div class="header-title">
                     <h1>Wonderlust</h1>
                     <h5>Explore the unexpected</h5>
                 </div>
             </div>
-        </header>
+        </div>
     )
 }
 
