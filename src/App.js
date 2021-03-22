4fef6cd8740e1f107b89e7f7f6e13e8635cc84b3
eribau <erik_bauer@hotmail.com>
2021-03-22 19:47:29 +0100

    Moved views from index.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import MapView from './views/mapView';
+import TitleView from './views/titelView';
+import SearchView from './views/searchView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TitleView/>
+      <SearchView/>
+      <MapView/>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.js'
 import reportWebVitals from './reportWebVitals';
-import MapView from './views/mapView';
-import TitleView from './views/titelView';
-import SearchView from './views/searchView';
 
 require('dotenv').config();
 ReactDOM.render(
   <React.StrictMode>
-    <TitleView/>
-    <SearchView/>
-    <MapView/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
