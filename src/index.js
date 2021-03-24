35b2506a269816db9d8c4a5df56d6e0a1101f147
eribau <erik_bauer@hotmail.com>
2021-03-24 17:43:13 +0100

    Added function for retrieving wiki-articles from coordinates

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App.js'
 import reportWebVitals from './reportWebVitals';
 
-require('dotenv').config();
 ReactDOM.render(
   <React.StrictMode>
     <App/>
