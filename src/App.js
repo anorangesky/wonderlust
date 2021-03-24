bb95c1fa445e32a16369f39f4e13cfe9262c4be7
agnespet <agnespet@kth.se>
2021-03-24 13:12:15 +0100

    .

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import MapView from './views/mapView';
 import TitleView from './views/titelView';
 import SearchView from './views/searchView';
+import DetailsView from './views/detailsView';
 
 function App() {
   return (
     <React.Fragment>
       <TitleView/>
       <SearchView/>
+      <DetailsView/>
       <MapView/>
     </React.Fragment>
   );
