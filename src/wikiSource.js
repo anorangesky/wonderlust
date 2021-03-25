626328fa11bbb2de085e134e1df6d333d17e6861
eribau <erik_bauer@hotmail.com>
2021-03-25 17:00:11 +0100

    Added function for getting article contents (text+image)

--- src/wikiSource.js
+++ src/wikiSource.js
@@ -1,4 +1,5 @@
 // https://en.wikipedia.org/w/api.php?action=query&format=json&list=geosearch&gscoord=37.7891838%7C-122.4033522&gsradius=10000&gslimit=100
+// https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cpageimages&pageids=3276454&formatversion=2&exlimit=1&exintro=1&explaintext=1&piprop=original
 
 function apiCall(params) {
   return fetch('https://en.wikipedia.org/w/api.php?' + params + '&origin=*')
@@ -11,8 +12,8 @@ function apiCall(params) {
   .then(response => response.json());
 }
 
-// Return a promise with an array of articles with the properties: {dist, lat, lon, ns, pageid, primary, title}
-export function articlesFromLocation(lat, lng, radius) {
+// Returns a promise with an array of articles with the properties: {dist, lat, lon, ns, pageid, primary, title}
+export function getArticlesFromLocation(lat, lng, radius) {
   var params = {
     action: 'query',
     list: 'geosearch',
@@ -22,4 +23,21 @@ export function articlesFromLocation(lat, lng, radius) {
   };
   return apiCall(new URLSearchParams(params))
           .then(data => data.query.geosearch);
+}
+
+// Returns a promise with an article with the properties: {pageid, ns, title, extract ,original}
+export function getArticleContent(id) {
+  var params = {
+    action: 'query',
+    format: 'json',
+    prop: 'extracts' + '|' + 'pageimages',
+    pageids: id.toString(),
+    formatversion: '2',
+    exlimit: '1',
+    exintro: '1',
+    explaintext: '1',
+    piprop: 'original',
+  }
+  return apiCall(new URLSearchParams(params))
+          .then(data => data.query.pages);
 }
