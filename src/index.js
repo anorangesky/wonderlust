2bd5e850f849cc90acf7175e3ab4991b81c1361c
eribau <erik_bauer@hotmail.com>
2021-03-25 10:52:48 +0100

    Added redux store and state for current location

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <App/>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
 
