f65c86b22a38fddef1d7727ef6fe8e57e99baab3
eribau <erik_bauer@hotmail.com>
2021-04-09 13:35:27 +0200

    moved the go-to-your-location button outside the searchbar

--- src/views/searchView.js
+++ src/views/searchView.js
@@ -3,15 +3,17 @@ import icon from '../images/location.png'
 
 function SearchView(props) {
   return(
-    <div className="search">
-      <input type="search"
-            id="locationSearch"
-            placeholder="Find a place"
-            onChange={e => props.onChange()}
-      />
-      <button onClick={e => props.onPress()}>
-        <img src={icon} alt="go to your location"/>
-      </button>
+    <div className="searchview">
+      <div className="searchbar">
+        <input type="search"
+              id="locationSearch"
+              placeholder="Find a place"
+              onChange={e => props.onChange()}
+        />
+      </div>
+        <button className="currentlocation-btn" onClick={e => props.onPress()}>
+          <img src={icon} alt="go to your location"/>
+        </button>
     </div>
   );
 }
